a684d5f3fb662e2903591331740db249576b1af6
竹内佑介 <kaidouji85@gmail.com>
2021-07-08 23:33:11 +0900

    コンソール出力を削除した

--- bubun-syugo.js
+++ bubun-syugo.js
@@ -1,24 +1,22 @@
+/** 集合 */
 const list = [1,2,3];
 
-function test(subset, data) {
-  console.log('call test');
-  console.log('subset', subset);
-  console.log('data', data);
+/**
+ * 部分集合を求める
+ *
+ * @param subsets 現在算出した部分集合の集まり
+ * @param data 集合の残り
+ * @return 更新した部分集合
+ */
+function test(subsets, data) {
   if (data.length === 0) {
-    console.log('zero');
-    return subset;
+    return subsets;
   }
 
   const top = data[0];
-  const addTopToExistSubsetMember = subset.map(v => [...v, top]);
-  const totalSubset = [...subset, [top], ...addTopToExistSubsetMember]
+  const addTopToExistSubsetMember = subsets.map(v => [...v, top]);
+  const totalSubset = [...subsets, [top], ...addTopToExistSubsetMember]
   const remainingData = data.slice(1);
-
-  console.log('top', top);
-  console.log('addTopToExistSubsetMember', addTopToExistSubsetMember);
-  console.log('totalSubset', totalSubset);
-  console.log('remainingData', remainingData);
-
   return test(totalSubset, remainingData);
 }
 
